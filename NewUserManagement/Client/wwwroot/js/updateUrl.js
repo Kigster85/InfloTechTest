671cec469e2866378096849966aedc71991285b3
Philip Billson <philipbillson85@gmail.com>
2024-02-21 00:52:34 +0000

    View and Edit Modals both working, started binding edit modal to the rigth things, need to sort out a put request to submit the save details to the cache and database on submit.

---
+++ NewUserManagement/Client/wwwroot/js/updateUrl.js
@@ -0,0 +1,8 @@
+﻿window.updateUrlWithoutNavigation = function (route, userId) {
+    // Construct the URL based on the route and userId
+    var url = userId ? `/list/${route}/${userId}` : `/list`;
+
+    // Update the URL without causing navigation
+    history.replaceState({}, "", url);
+};
+
